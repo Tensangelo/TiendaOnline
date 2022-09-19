cc7dd64c7477586a2d6cc5b21800be7c32d05d82
Angelo Gaona <web_mateo@hotmail.com>
2022-09-19 06:01:45 -0500

    Fixed APi getStaticProps

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -4,30 +4,48 @@ import ProductDetails from "@components/products/ProductDetails";
 
 const { URL } = process.env;
 
-export async function getStaticPaths() {
-    const response = await fetch (URL + `/api/products/allProducts`);
-    const { data }: TAPIProductResponse = await response.json();
+// export async function getStaticPaths() {
+//     const response = await fetch (URL + `/api/products/allProducts`);
+//     const { data }: TAPIProductResponse = await response.json();
 
-    const paths = data.map(({ id }) => ({ params: { id } }))
+//     const paths = data.map(({ id }) => ({ params: { id } }))
 
-    return {
-        paths,
-        fallback: false,
-    }
-}
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+// }
 
-export async function getStaticProps({ params }: any) {
-    const response = await fetch (URL + `/api/products/${params?.id}`)
+// export async function getStaticProps({ params }: any) {
+//     const response = await fetch (URL + `/api/products/${params?.id}`)
 
+//     const product = await response.json();
+//     return { props: { product } }
+// }
+
+const FetchAPI = async ({id} : any) => {
+    const response = await fetch(URL + `/api/products/${id}`)
     const product = await response.json();
+
     return { props: { product } }
 }
 
-const ProductId = ({ product }: { product: TProduct }) => {
+export async function getServerSideProps({query}:any) {
+    const initialResult = await (FetchAPI(query));
+
+    return {
+        props: {
+            initialResult,
+        },
+    }
+}
+
+const ProductId = ({ initialResult }: any) => {
+    const Data = initialResult.props?.product;
 
     return (
         <>
-            {product == null ? null : <ProductDetails product={product} />}
+            {Data == null ? null : <ProductDetails product={Data} />}
         </>
     )
 }
